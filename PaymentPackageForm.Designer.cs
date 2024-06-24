be13fae84c0244e559c1e27e961a7473698b9692
Lui Peng <norngliipeng98.info@gmail.com>
2024-06-24 08:44:00 +0700

    add package form

--- PaymentPackageForm.Designer.cs
+++ PaymentPackageForm.Designer.cs
@@ -28,35 +28,334 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dateTimePicker1 = new DateTimePicker();
+            DataBus = new DataGridView();
+            label14 = new Label();
+            btnNew = new Button();
+            btnUpdate = new Button();
+            btnSearch = new Button();
+            btnSave = new Button();
+            txtSearch = new TextBox();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            textBox2 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            cmDriver = new ComboBox();
+            txtFullName = new TextBox();
+            label3 = new Label();
+            label6 = new Label();
+            label2 = new Label();
+            txtAmount = new TextBox();
+            label16 = new Label();
+            txtPayID = new TextBox();
+            label11 = new Label();
             label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)DataBus).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(864, 210);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(299, 27);
+            dateTimePicker1.TabIndex = 84;
+            // 
+            // DataBus
+            // 
+            DataBus.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataBus.Location = new Point(112, 667);
+            DataBus.Name = "DataBus";
+            DataBus.RowHeadersWidth = 51;
+            DataBus.RowTemplate.Height = 29;
+            DataBus.Size = new Size(1070, 280);
+            DataBus.TabIndex = 83;
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label14.Location = new Point(150, 630);
+            label14.Name = "label14";
+            label14.Size = new Size(55, 20);
+            label14.TabIndex = 82;
+            label14.Text = "Search";
+            // 
+            // btnNew
+            // 
+            btnNew.BackColor = Color.FromArgb(192, 255, 255);
+            btnNew.Location = new Point(1030, 953);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(152, 50);
+            btnNew.TabIndex = 81;
+            btnNew.Text = "NEW";
+            btnNew.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = Color.FromArgb(255, 192, 128);
+            btnUpdate.Location = new Point(717, 953);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(152, 50);
+            btnUpdate.TabIndex = 80;
+            btnUpdate.Text = "UPDATE";
+            btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = SystemColors.ActiveCaption;
+            btnSearch.Location = new Point(394, 953);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(152, 50);
+            btnSearch.TabIndex = 79;
+            btnSearch.Text = "SEARCH";
+            btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.FromArgb(192, 192, 255);
+            btnSave.Location = new Point(112, 953);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(152, 50);
+            btnSave.TabIndex = 78;
+            btnSave.Text = "SAVE";
+            btnSave.UseVisualStyleBackColor = false;
+            // 
+            // txtSearch
+            // 
+            txtSearch.BorderStyle = BorderStyle.FixedSingle;
+            txtSearch.Location = new Point(247, 603);
+            txtSearch.Multiline = true;
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(555, 47);
+            txtSearch.TabIndex = 77;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(cmDriver);
+            panel1.Controls.Add(txtFullName);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label6);
+            panel1.Location = new Point(141, 375);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1022, 172);
+            panel1.TabIndex = 76;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(733, 103);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(237, 34);
+            textBox1.TabIndex = 61;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(541, 117);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 20);
+            label4.TabIndex = 60;
+            label4.Text = "Ticket Qty";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(733, 29);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(237, 28);
+            comboBox1.TabIndex = 59;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(733, 63);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(237, 34);
+            textBox2.TabIndex = 57;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(541, 77);
+            label7.Name = "label7";
+            label7.Size = new Size(119, 20);
+            label7.TabIndex = 56;
+            label7.Text = "Customer Name";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(541, 37);
+            label8.Name = "label8";
+            label8.Size = new Size(93, 20);
+            label8.TabIndex = 55;
+            label8.Text = "Customer ID";
+            // 
+            // cmDriver
+            // 
+            cmDriver.FormattingEnabled = true;
+            cmDriver.Location = new Point(244, 29);
+            cmDriver.Name = "cmDriver";
+            cmDriver.Size = new Size(237, 28);
+            cmDriver.TabIndex = 53;
+            // 
+            // txtFullName
+            // 
+            txtFullName.Location = new Point(244, 63);
+            txtFullName.Multiline = true;
+            txtFullName.Name = "txtFullName";
+            txtFullName.Size = new Size(237, 34);
+            txtFullName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(52, 77);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Ticket Qty";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(52, 37);
+            label6.Name = "label6";
+            label6.Size = new Size(68, 20);
+            label6.TabIndex = 0;
+            label6.Text = "Ticket ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(727, 215);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 20);
+            label2.TabIndex = 75;
+            label2.Text = "Payment Date";
+            // 
+            // txtAmount
+            // 
+            txtAmount.BorderStyle = BorderStyle.FixedSingle;
+            txtAmount.Location = new Point(278, 265);
+            txtAmount.Multiline = true;
+            txtAmount.Name = "txtAmount";
+            txtAmount.Size = new Size(299, 44);
+            txtAmount.TabIndex = 74;
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label16.Location = new Point(141, 279);
+            label16.Name = "label16";
+            label16.Size = new Size(64, 20);
+            label16.TabIndex = 73;
+            label16.Text = "Amount";
+            // 
+            // txtPayID
+            // 
+            txtPayID.BorderStyle = BorderStyle.FixedSingle;
+            txtPayID.Location = new Point(278, 201);
+            txtPayID.Multiline = true;
+            txtPayID.Name = "txtPayID";
+            txtPayID.ReadOnly = true;
+            txtPayID.Size = new Size(299, 44);
+            txtPayID.TabIndex = 72;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(141, 215);
+            label11.Name = "label11";
+            label11.Size = new Size(88, 20);
+            label11.TabIndex = 71;
+            label11.Text = "Payment ID";
             // 
             // label1
             // 
-            label1.Anchor = AnchorStyles.None;
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            label1.Location = new Point(498, 273);
+            label1.Font = new Font("Heebo", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(577, 99);
             label1.Name = "label1";
-            label1.Size = new Size(161, 46);
-            label1.TabIndex = 2;
-            label1.Text = "Payment";
+            label1.Size = new Size(248, 44);
+            label1.TabIndex = 70;
+            label1.Text = "Payment's Form";
             // 
-            // PaymentForm
+            // PaymentPackageForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1112, 593);
+            ClientSize = new Size(1295, 1102);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(DataBus);
+            Controls.Add(label14);
+            Controls.Add(btnNew);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnSearch);
+            Controls.Add(btnSave);
+            Controls.Add(txtSearch);
+            Controls.Add(panel1);
+            Controls.Add(label2);
+            Controls.Add(txtAmount);
+            Controls.Add(label16);
+            Controls.Add(txtPayID);
+            Controls.Add(label11);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
-            Name = "PaymentForm";
+            Name = "PaymentPackageForm";
             Text = "PaymentForm";
+            ((System.ComponentModel.ISupportInitialize)DataBus).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
+        private DateTimePicker dateTimePicker1;
+        private DataGridView DataBus;
+        private Label label14;
+        private Button btnNew;
+        private Button btnUpdate;
+        private Button btnSearch;
+        private Button btnSave;
+        private TextBox txtSearch;
+        private Panel panel1;
+        private TextBox textBox1;
+        private Label label4;
+        private ComboBox comboBox1;
+        private TextBox textBox2;
+        private Label label7;
+        private Label label8;
+        private ComboBox cmDriver;
+        private TextBox txtFullName;
+        private Label label3;
+        private Label label6;
+        private Label label2;
+        private TextBox txtAmount;
+        private Label label16;
+        private TextBox txtPayID;
+        private Label label11;
         private Label label1;
     }
 }
